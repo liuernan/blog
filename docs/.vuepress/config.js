1e7dc7f92e9dc0086cde4736537d48ce86a6c5a1
Jiawei <liujiaweidreamer@gmail.com>
2020-06-29 19:45:43 +0800

    add .vuepress/config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title: '二男的博客',
+  description: '编程语言 编程思想',
+  base: '/blog/',
+  cache: false,
+  themeConfig: {
+    // logo: '/assets/img/logo.png',
+    lastUpdated: 'Last Updated',
+    nav: [
+      {text: 'Docs', link: '/'},
+      {text: 'Github', link: 'https://github.com/liuernan/blog'}
+    ],
+    sidebar: [
+      {
+        title: '编程语言',
+        collapsable: false,
+        children: []
+      },
+      {
+        title: '编程思想',
+        collapsable: false,
+        children: [
+          'thoughts/MVC.md'
+        ]
+      }
+    ]
+  }
+}
